1ad1637cb06103a06b466d832f83cbbeaa9be862
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-08-22 04:47:04 +0000

    Fixed Error in doubly linked list

---
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *saved_head;
+	dlistint_t *tmp, *tmp_1;
+	unsigned int p;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		tmp = (*head)->prev;
+		tmp_1 = (*head)->next;
+		tmp->next = tmp_1;
+		free(*head);
+		if (tmp_1)
+			tmp_1->prev = tmp;
+		*head = saved_head;
+	}
+	return (1);
+}
